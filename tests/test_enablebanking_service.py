6148f86ff679e80e3b934945988f91052702376f
ivan-holvi <ijmartin@holvi.com>
2025-04-07 13:58:05 +0200

    chore: format

--- tests/test_enablebanking_service.py
+++ tests/test_enablebanking_service.py
@@ -3,7 +3,9 @@ from datetime import date, datetime
 from unittest import mock
 
 from src.enablebanking_sdk.constants import PSUType
-from src.enablebanking_sdk.constants.transaction_fetch_strategy import TransactionsFetchStrategy
+from src.enablebanking_sdk.constants.transaction_fetch_strategy import (
+    TransactionsFetchStrategy,
+)
 from src.enablebanking_sdk.models import AspspData, Transaction
 from src.enablebanking_sdk.service import EnableBankingService, EnableBankingIntegration
 from tests.utils import get_json_fixtures
